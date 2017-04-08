5308bb82f65eb3147ecff2ee007422ec13673ffd
colinmiller <colinchmiller@gmail.com>
2017-04-08 13:38:33 -0500

    basic button styling and function

--- server/public/assets/scripts/mainApp.js
+++ server/public/assets/scripts/mainApp.js
@@ -2,4 +2,8 @@ var mainApp = angular.module('mainApp', []);
 
 mainApp.controller('MainController', ['$scope', function($scope){
   console.log("MainController is running");
+
+  $scope.toggleHeight = function(){
+    console.log("clicking it, bitch!");
+  }
 }]);
