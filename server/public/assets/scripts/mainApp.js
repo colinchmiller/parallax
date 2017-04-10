bd2a35bfdebfec5cb5f0841e8f136aba75e8fc43
colinmiller <colinchmiller@gmail.com>
2017-04-10 12:34:40 -0500

    button function to toggle class

--- server/public/assets/scripts/mainApp.js
+++ server/public/assets/scripts/mainApp.js
@@ -3,6 +3,6 @@ var mainApp = angular.module('mainApp', []);
 mainApp.controller('MainController', ['$scope', function($scope){
   $scope.activeClass = true;
   $scope.toggleHeight = function(){
-    $scope.activeClass = !scope.activeClass;
+    $scope.activeClass = !$scope.activeClass;
   }
 }]);
