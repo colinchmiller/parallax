d3baa71789775459c2c98a8d111276711a16eeb7
colinmiller <colinchmiller@gmail.com>
2017-04-17 22:24:23 -0500

    heroku push with new port setting

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT));
 
 app.get('/*', function(req, res){
   var file = req.params[0] || 'assets/views/index.html';
